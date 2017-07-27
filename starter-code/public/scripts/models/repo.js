26baaf022e3c62a09019c38279b0cd7f39f21a7a
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-07-27 14:55:15 -0500

    repo.js comment

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -5,7 +5,8 @@ var app = app || {};
   const repos = {};
   repos.all = [];
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // : What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // This function makes and ajas get requests using the app.get route for /github/ puts the returned data into repos.all, and fires off a callback function.  It is called in aboutController.js and passed the app.repoView.index as it's callback function.
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
       .then(data => repos.all = data, err => console.error(err))
