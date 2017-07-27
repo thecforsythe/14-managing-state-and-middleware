a1be16a1b65b1893c5b3a06c1323393f771cf1c5
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-07-27 15:04:16 -0500

    repoView.js comment

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,8 +4,8 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
-  // (put your response in a comment here)
+  // : What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // This function shows the element with id = "about" and hides all siblings of that element.  Then it calls the requestRepos function from repo.js and passes in the repoView.index function as the callback. (repoView.index appends data).
   aboutController.index = () => {
     $('#about').show().siblings().hide();
     app.repos.requestRepos(app.repoView.index);
--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -13,7 +13,8 @@ var app = app || {};
 
   const render = Handlebars.compile($('#repo-template').text());
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // : What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // this function calls the ui function (above), which removes all the children of a <ul> element, shows the element with id = about and hides it's siblings.  Then repoView.index calls the repos.with function (from repo.js) to get any items from repos.all that have the name key, usues the render function (above) (Handlebars.compile($('#repo-template').text())) to build some HTML and appends each of those into the ul in the #about element.
   repoView.index = function() {
     ui();
 
